a96d6ff800a3c1677b295a706ad4ca40170128a6
YamilLopezz <89939184+YamilLopezz@users.noreply.github.com>
2021-11-29 15:23:04 -0500

    Update script.js

--- homework/homework12/script.js
+++ homework/homework12/script.js
@@ -4,3 +4,10 @@ let msg = document.querySelector('#message');
 button.addEventListener('click', ()=>{
   msg.classList.toggle('reveal');
 })
+
+let second = document.querySelector('#another');
+let message = document.querySelector('#message1');
+
+second.addEventListener('click', function(){
+   message.classList.toggle('reveal1');
+})
